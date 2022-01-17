b9e7010a78e563754ac4d22f58e7cce4cf62b2ee
LuciaFerreras <97744416+LuciaFerreras@users.noreply.github.com>
2022-01-17 21:48:23 +0100

    Add files via upload

---
+++ Punto1/main.java
@@ -0,0 +1,41 @@
+package grupoAD.appconcurso;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author usuario
+ */
+public class main
+{
+    public static void main(String[] args)
+    {
+        Connection con = Conexion.getConnection();
+
+        if (con != null) {
+
+            Punto1 p = new Punto1((java.sql.Connection) con);
+            Scanner tec = new Scanner(System.in);
+            int n = 0;
+            boolean acceso;
+            
+            System.out.println("Usuario: ");
+            String usu = tec.nextLine();
+            System.out.println("Contraseña: ");
+            String pwd = tec.nextLine();
+            acceso = p.validarUsuario(usu, pwd);
+            if (acceso) {
+                System.out.println("Iniciada sesion para " + usu);
+            } else {
+                System.out.println("Error en credenciales");
+            }
+        }
+    }
+
+
+}
